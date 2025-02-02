db0bf3a01918342394cb278cde604178737d5aa2
adrianofpereira <nanojau@gmail.com>
2025-02-02 14:12:40 -0300

    Conexão e Atualização do Banco de Dados com Firebase

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        DatabaseReference usuarios = referencia.child("usuarios");
+
+        Usuario usuario = new Usuario();
+        usuario.setNome("Jamilon");
+        usuario.setSobrenome("");
+        usuario.setIdade(30);
+
+        usuarios.child("001").setValue("usuarios");
+
+    }
+}
